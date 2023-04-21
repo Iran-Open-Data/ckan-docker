bff542f2e2254f919f1518916dbf4cccc6c69df8
TerseTears <TerseTears@protonmail.com>
2023-04-21 11:03:40 +0330

    Modify init.sh

--- ckan/docker-entrypoint.d/init.sh
+++ ckan/docker-entrypoint.d/init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # ckan config-tool /srv/app/ckan.ini "ckan.site_url = https://data.gsmedb.ir"
+ckan config-tool /srv/app/ckan.ini "ckan.site_title = SDAC CKAN"
+ckan config-tool /srv/app/ckan.ini "ckan.site_description = CKAN repository for Sharif Data Analysis Center"
+ckan config-tool /srv/app/ckan.ini "ckan.site_intro_text = Welcome to CKAN for Sharif Data Analysis Center"
+ckan config-tool /srv/app/ckan.ini "ckan.locales_offered = en fa_IR"
+ckan config-tool /srv/app/ckan.ini "ckan.locale_order = en fa_IR"
+ckan config-tool /srv/app/ckan.ini "ckan.display_timezone = Asia/Tehran"
