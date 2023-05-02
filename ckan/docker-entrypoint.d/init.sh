47a47c6a0a5105d47a7e794a5c16161e3adfc1b6
TerseTears <TerseTears@protonmail.com>
2023-05-02 20:27:00 +0330

    Add activity extension

--- ckan/docker-entrypoint.d/init.sh
+++ ckan/docker-entrypoint.d/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ckan config-tool /srv/app/ckan.ini "ckan.site_url = https://data.gsmedb.ir"
-ckan config-tool /srv/app/ckan.ini "ckan.plugins = envvars image_view text_view recline_view datastore datapusher example_theme showcase hierarchy_display hierarchy_form hierarchy_group_form"
+ckan config-tool /srv/app/ckan.ini "ckan.plugins = envvars image_view text_view recline_view datastore datapusher activity example_theme showcase hierarchy_display hierarchy_form hierarchy_group_form"
 
 ckan config-tool /srv/app/ckan.ini "ckan.site_title = SDAC CKAN"
 ckan config-tool /srv/app/ckan.ini "ckan.site_description = CKAN repository for Sharif Data Analysis Center"
@@ -10,4 +10,6 @@ ckan config-tool /srv/app/ckan.ini "ckan.locales_offered = en fa_IR"
 ckan config-tool /srv/app/ckan.ini "ckan.locale_order = en fa_IR"
 ckan config-tool /srv/app/ckan.ini "ckan.display_timezone = Asia/Tehran"
 ckan config-tool /srv/app/ckan.ini "ckan.auth.public_activity_stream_detail = true"
+ckan config-tool /srv/app/ckan.ini "ckan.activity_streams_email_notifications = true"
 ckan config-tool /srv/app/ckan.ini "ckan.auth.allow_dataset_collaborators = true"
+
